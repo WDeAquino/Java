6e0737d595b55b13a8ff97b6a3d41d69bd28b1b2
WDeAquino <51212952+WDeAquino@users.noreply.github.com>
2021-02-05 18:51:17 -0600

    Create 3- Scanner.java

---
+++ 3- Scanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P4{
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		String nombre= "";
+		int num1=0,num2=0;
+		System.out.println("¿Cual es tu nombre?");
+		nombre = in.nextLine();
+		System.out.println("Dame un numero: ");
+		num1= in.nextLine();
+		System.out.println("Dame otro numero: ");
+		num2= in.nextLine();
+		System.out.println("Hola "+nombre+" el resultado de la suma es: "+num1+num2);
+	}
+}
